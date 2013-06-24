4b9d3cac124ea1bdf85a16165b827aac0f2e6b09
Ned Ruggeri <ruggeri@self-loop.com>
2013-06-24 11:22:45 -0700

    setup initial models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130620163148) do
+ActiveRecord::Schema.define(:version => 20130624181726) do
 
   create_table "secrets", :force => true do |t|
     t.string   "title",        :null => false
@@ -20,6 +20,21 @@ ActiveRecord::Schema.define(:version => 20130620163148) do
     t.integer  "recipient_id", :null => false
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "todo_items", :force => true do |t|
+    t.string   "title",        :null => false
+    t.integer  "todo_list_id", :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  add_index "todo_items", ["todo_list_id"], :name => "index_todo_items_on_todo_list_id"
+
+  create_table "todo_lists", :force => true do |t|
+    t.string   "title",      :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
